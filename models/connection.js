3687f5cceadca452d29a6a497bc66743c9b88783
Silvia Kocsisova <silviakocsisovaCB5@MacBook-Air-3.local>
2019-08-21 12:40:45 -0400

    Project 002 started

---
+++ models/connection.js
@@ -0,0 +1,38 @@
+/* Import the mongoose module
+ *
+ */
+const mongoose = require('mongoose');
+
+/* Step 1.
+ *
+ * TODO: replace <db-name> with the name of your mongo database. 
+ * This will need to change for every new project you create.
+ *
+ */
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost/IssueTracker";
+// name of local database is IssueTracker
+// this: process.env.MONGODB_URI is database in cloud
+// this "mongodb://localhost/IssueTracker" is database on local machine.
+//     ||     or means if cloud database is not available, then local db is used
+
+
+
+/* Step 2
+ *
+ * Open up a connection to the mongo database.
+ *
+ * NOTE: newUrlParser diables a deprecation warning
+ */
+mongoose.connect(connectionString, { useNewUrlParser: true})
+  .then(() => {
+    console.log("connected to mongo at: " + connectionString);
+  });
+
+
+/* Export the mongoose object.
+ *
+ * This will allow us to use the same connection to our DB
+ * across our different controllers.
+ *
+ */
+module.exports = mongoose
---
+++ readme.md
@@ -0,0 +1,27 @@
+
+# Immunization Register
+
+## A full CRUD MEHN (Mongo, Express, Handlebars, Node) WebApp
+
+MongoDB CRUD Operations:
+<ul>
+<li>Create</li>
+<li>Read</li>
+<li>Update</li>
+<li>Delete</li>
+</ul>
+
+#### MongoDB Schema:
+<img src="Schema_ImmunizationRegister.png" alt="Schema Immunization Register">
+
+Types of Vaccines:
+<ul>
+<li>Live, attenuated</li>
+<li>Inactivated/Killed</li>
+<li>Toxoid (inactivated toxin)</li>
+<li>DSubunit/conjugate</li>
+</ul>
+
+
+
+
